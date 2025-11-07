798e0c5d8d1163de74c24bb830ce1c646b550ee1
ccr-515 <84052709+ccr-515@users.noreply.github.com>
2025-11-07 01:48:27 -0600

    Delete sw.js

--- sw.js
+++
@@ -1,1 +0,0 @@
-self.addEventListener('install',e=>{e.waitUntil(caches.open('ifm-v1').then(c=>c.addAll(['./','./index.html','./styles.css','./app.js','./data/locations.json'])));});self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
